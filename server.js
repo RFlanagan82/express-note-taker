800b4196841226f7ee8e130117bbd1e7a4fb2721
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-30 18:24:48 -0400

    fixed connectivity issues for server.js to index.html and notes.html and formatted posting of notes into the note taker. Current issue is that each new note overrides the existing note.

--- server.js
+++ server.js
@@ -1,69 +1,76 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
+const app = express();
 //creates a port for the process to listen on
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 // Notes Data from db.json
 // =============================================================
 
-require("./develop/db/db.json")(app);
-
-
-// Need an array to create note ids that will be used to delete them later
-// =============================================================
-noteId = []
-
-
-// HTML Routes
-// =============================================================
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
-
+const noteTable = "db/db.json";
+//const noteTable2 = require("./db/db.json");
 
 // API Routes
 // =============================================================
 
 //Reads db.json file and displays all notes as JSON
-// app.get("/api/notes", function(req, res){
-//     res.readFile(db.json);
-//     return res.json(db.json);
-// });
+app.get("/api/notes", function(req, res){
+    fs.readFile(noteTable, "utf8", function(err, log){
+        console.log(log)
+        return res.sendFile(path.join(__dirname, "db/db.json"));
+    });
+    
+});
 
 
 //Should receive a new note to save on request body, add it to db.json file and return the new note to the client.
-// app.post("/api/notes", function(req, res){
-//     const newNote = req.body;
-//     const noteId = newNote[i];
+app.post("/api/notes", function(req, res){
+    //automatically destructures the array into strings and getting it from the body
+    const {title, text} = req.body;
+    const id = Math.ceil(Math.random() * 1000)
+    const newNote = {title, text, id}
+    
+    let noteArray = []
+    fs.readFile(noteTable, "utf8", function(err, log){
+        console.log("checking log" + log)
+        noteArray = log || [];
+    
+    });
+ 
+    console.log(noteArray);
+    noteArray.push(newNote);
 
-//     console.log(newNote, noteId);
+    fs.writeFile(noteTable, JSON.stringify(noteArray), function(err){
+        if(err) throw err;
+        return res.sendFile(path.join(__dirname, "db/db.json"));
+    });
 
-//     db.json.push(newNote);
-
-//     res.json(newNote);
-// })
-
-
+})
 
 
 
+// HTML Routes
+// =============================================================
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
 // Starts the server to begin listening
