d15a11c8e7dd4d246bc2db640ccee0e4e507148a
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-01 18:11:39 -0400

    finished troubleshooting issues with app.delete.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
+
 //creates a port for the process to listen on
 const PORT = process.env.PORT || 3000;
 
@@ -16,60 +17,77 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-// Notes Data from db.json
+// HTML Routes
 // =============================================================
 
-const noteTable = "db/db.json";
-//const noteTable2 = require("./db/db.json");
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
 // API Routes
 // =============================================================
 
 //Reads db.json file and displays all notes as JSON
 app.get("/api/notes", function(req, res){
-    fs.readFile(noteTable, "utf8", function(err, log){
-        console.log(log)
-        return res.sendFile(path.join(__dirname, "db/db.json"));
+    return res.sendFile(path.join(__dirname, "db/db.json"));
     });
-    
-});
 
 
 //Should receive a new note to save on request body, add it to db.json file and return the new note to the client.
 app.post("/api/notes", function(req, res){
     //automatically destructures the array into strings and getting it from the body
     try{
-        newNote = fs.readFileSync("db/db.json", "utf8");
+        //new note needs to be read into the db.json array
+        newNote = fs.readFileSync("./db/db.json", "utf8");
+        console.log(newNote)
+        //need to parse the values for the new Note 
         newNote = JSON.parse(newNote)
+        //Create a new id for each note
         req.body.id = Math.ceil(Math.random() * 1000)
+        //need to push the new note into the request body
         newNote.push(req.body);
+        //need to stringify the newNote to be placed in the db.json array
         newNote = JSON.stringify(newNote);
-        fs.writeFile("db/db.json", newNote, "utf8",function(err){
+        //write the new note to the db.json array
+        fs.writeFile("./db/db.json", newNote, "utf8",function(err){
+            if(err) throw err;
+        });
+        //provide the result for the user by parsing the new note values back
+        res.json(JSON.parse(newNote));
+    }   catch (error) {
+        //console.log("Note wasn't added.")
+        throw err;
+    }
+});
+
+//grab the id created from the app.post(api/notes)function
+app.delete("/api/notes/:id", function(req,res){
+
+    try{
+        //new note needs to be read by Filesystem package
+        newNote = fs.readFileSync("./db/db.json", "utf8");
+        //need to parse the newNote entry
+        newNote = JSON.parse(newNote)
+        //need to filter the entries by ID not by index order
+        newNote = newNote.filter(function(note){
+            return note.id != req.params.id
+        });
+        //need to stringify the note
+        newNote = JSON.stringify(newNote)
+        //need to update the file to remove the note
+        fs.writeFile("./db/db.json", newNote, "utf8", function(err){
             if(err) throw err;
         });
         res.json(JSON.parse(newNote));
-    } catch (error) {
-        console.log("Note wasn't added.")
+
+    } catch (err) {
+        throw err;
     }
 });
  
-    
-
-
-
-
-
-// HTML Routes
-// =============================================================
-
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
 
 // Starts the server to begin listening
 // =============================================================
